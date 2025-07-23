4b20f3460cbbe318ee0e0de8bd7faa3d566e71de
Milnandy <arrafii1910@gmail.com>
2025-07-23 11:23:17 +0700

    feat(contact): styling with tailwind

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-// hamburger
-const hamburger = document.querySelector('#hamburger');
-
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-});
-
 // logo condition
 window.onscroll = function() {
     const logoSmall = document.querySelector('#logo-small');
@@ -15,11 +8,13 @@ window.onscroll = function() {
         // Tampilkan logo kecil dan sembunyikan logo besar
         logoSmall.classList.remove('hidden');
         logoSmall.classList.add('block');
+        logo.classList.remove('block');
         logo.classList.add('hidden');
     } else {
         // Kembalikan ke kondisi awal jika scroll kembali ke atas
         logoSmall.classList.remove('block');
         logoSmall.classList.add('hidden');
         logo.classList.remove('hidden');
+        logo.classList.add('block');
     }
 }
